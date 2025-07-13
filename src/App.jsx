b868df1f73c3add6f6f64545d4979aa965a72f45
rinu1326 <nifu4040@gmail.com>
2025-07-13 16:23:47 +0530

    Added AOS animations and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
-   <>
-   <Navbar/>
-   <Home/>
-   </>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      startEvent: 'DOMContentLoaded',
+    });
 
-export default App
+    // Refresh AOS just in case on initial mount
+    setTimeout(() => {
+      AOS.refresh();
+    }, 100);
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+};
+
+export default App;
